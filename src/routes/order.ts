91900e688540ba44dd37d56d9ccde17d06e4d848
Emma Mora <emmanu20021@gmail.com>
2024-01-04 21:35:06 -0600

    Endpoints de pedido

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import { addOrder, getOrder, getOrders, removeOrder, updateOrder } from '../controller/order';
+import { addOrder, getOrder, getOrders, getOrdersUser, removeOrder, updateOrder } from '../controller/order';
 
 const router = express.Router();
 
 router.get('/', getOrders);
 
 router.get('/:id', getOrder);
+
+router.get('/user/:id', getOrdersUser);
 
 router.post('/add', addOrder);
 
